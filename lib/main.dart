45438533852851ac059cbaaee3d8a2d0184c66fa
ryuji <ryujispjp@gmail.com>
2022-08-07 21:09:16 +0900

    freezedを使用してcountUp,countDown実装 ロジックとviewの切り離しの途中

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,105 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_practice/Data/count_data.dart';
 import 'package:flutter_practice/provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   runApp(
-    ProviderScope(child: const MyApp()),
+    ProviderScope(child: MyApp()),
   );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  MyHomePage({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Consumer(
-            builder: (context, ref, child) => Text(ref.watch(titleProvider))),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Consumer(
-              builder: (context, ref, child) => Text(
-                ref.watch(messageProvider),
-              ),
-            ),
-            Consumer(
-              builder: (context, ref, child) => Text(
-                ref.watch(countProvider).toString(),
-                style: Theme.of(context).textTheme.headline4,
-              ),
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: Consumer(
+              builder: (context, ref, child) => Text(ref.watch(titleProvider))),
         ),
-      ),
-      floatingActionButton: Consumer(
-        builder: (context, ref, child) => FloatingActionButton(
-          onPressed: () =>
-              ref.read(countProvider.state).update((state) => state + 1),
-          tooltip: 'Increment',
-          child: const Icon(Icons.add),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Consumer(
+                builder: (context, ref, child) => Text(
+                  ref.watch(messageProvider),
+                ),
+              ),
+              Consumer(
+                builder: (context, ref, child) => Text(
+                  ref.watch(countDataProvider.state).state.count.toString(),
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Consumer(
+                    builder: (context, ref, child) => FloatingActionButton(
+                      onPressed: () {
+                        CountData countData =
+                            ref.read(countDataProvider.state).state;
+                        ref.read(countDataProvider.state).state =
+                            countData.copyWith(
+                                count: countData.count - 1,
+                                countDown: countData.countDown + 1);
+                      },
+                      child: const Icon(CupertinoIcons.minus),
+                    ),
+                  ),
+                  Consumer(
+                    builder: (context, ref, child) => FloatingActionButton(
+                      onPressed: () {
+                        CountData countData =
+                            ref.read(countDataProvider.state).state;
+                        ref.read(countDataProvider.state).state =
+                            countData.copyWith(
+                                count: countData.count + 1,
+                                countUp: countData.countUp + 1);
+                      },
+                      child: const Icon(CupertinoIcons.plus),
+                    ),
+                  ),
+                ],
+              ),
+              Consumer(
+                builder: (context, ref, child) => Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text(
+                      ref
+                          .watch(countDataProvider.state)
+                          .state
+                          .countDown
+                          .toString(),
+                    ),
+                    Text(
+                      ref
+                          .watch(countDataProvider.state)
+                          .state
+                          .countUp
+                          .toString(),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: Consumer(
+          builder: (context, ref, child) => FloatingActionButton(
+            onPressed: () {
+              ref.read(countDataProvider.state).state = CountData(
+                count: 0,
+                countDown: 0,
+                countUp: 0,
+              );
+            },
+            child: const Icon(CupertinoIcons.refresh),
+          ),
         ),
       ),
     );
--- lib/provider.dart
+++ lib/provider.dart
@@ -1,5 +1,6 @@
 import 'dart:ffi';
 
+import 'package:flutter_practice/Data/count_data.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final titleProvider = Provider<String>((ref) {
@@ -11,3 +12,6 @@ final messageProvider = Provider<String>((ref) {
 });
 
 final countProvider = StateProvider<int>((ref) => 0);
+final countDataProvider = StateProvider<CountData>((ref) {
+  return CountData(count: 0, countUp: 0, countDown: 0);
+});
